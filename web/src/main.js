81e0c85809105b963d32a77a44a008fcf94e9150
zhang-xiao-hao <2314606193@qq.com>
2023-08-31 16:53:04 +0800

    前端集成Ant design view及其图标库

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 前端组件框架
+import Antd from 'ant-design-vue';
+// 图标库
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).use(Antd).mount('#app')
+
+// 全局使用图标库
+const icons = Icons
+for (const i in icons){
+    app.component(i, icons[i])
+}
