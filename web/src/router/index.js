a30cff4ff70d21ee0a276f0d95884b12a0735b6d
zhang-xiao-hao <2314606193@qq.com>
2023-09-01 20:20:44 +0800

    增加前端页面跳转登录拦截，访问控制台所有页面都需要登录

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/store";
+import {notification} from "ant-design-vue";
 
 const routes = [
   {
@@ -7,7 +9,10 @@ const routes = [
   },
   {
     path: '/',
-    component: () => import(/* webpackChunkName: "about" */ '../views/main.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/main.vue'),
+    meta: {
+      loginRequire: true
+    }
   }
 ]
 
@@ -15,5 +20,27 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+//路由的登录拦截
+//页面跳转之前做登录校验
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+    return item.meta.loginRequire //当item.meta.loginRequire为true时，执行下面的代码
+  })) {
+    const _member = store.state.member;
+    console.log("页面登录校验开始：", _member);
+    if (!_member.token) {
+      console.log("用户未登录或登录超时！");
+      notification.error({ description: "未登录或登录超时" });
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -29,7 +29,6 @@ export default defineComponent({
   },
   setup() {
     return {
-
     };
   },
 });
