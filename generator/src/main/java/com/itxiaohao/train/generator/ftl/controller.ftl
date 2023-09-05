501821c53b5aec4b7773aa0d0e095528171503be
zhang-xiao-hao <2314606193@qq.com>
2023-09-05 14:17:40 +0800

    使用PassengerController制作模板，生成controller成功

--- generator/src/main/java/com/itxiaohao/train/generator/ftl/controller.ftl
+++ generator/src/main/java/com/itxiaohao/train/generator/ftl/controller.ftl
@@ -3,33 +3,33 @@ package com.itxiaohao.train.member.controller;
 import com.itxiaohao.train.common.context.LoginMemberContext;
 import com.itxiaohao.train.common.resp.CommonResp;
 import com.itxiaohao.train.common.resp.PageResp;
-import com.itxiaohao.train.member.req.PassengerQueryReq;
-import com.itxiaohao.train.member.req.PassengerSaveReq;
-import com.itxiaohao.train.member.resp.PassengerQueryResp;
-import com.itxiaohao.train.member.service.PassengerService;
+import com.itxiaohao.train.member.req.${Domain}QueryReq;
+import com.itxiaohao.train.member.req.${Domain}SaveReq;
+import com.itxiaohao.train.member.resp.${Domain}QueryResp;
+import com.itxiaohao.train.member.service.${Domain}Service;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/passenger")
-public class PassengerController {
+@RequestMapping("/${do_main}")
+public class ${Domain}Controller {
     @Resource
-    private PassengerService passengerService;
+    private ${Domain}Service ${domain}Service;
 
     @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
-        passengerService.save(req);
+    public CommonResp<Object> save(@Valid @RequestBody ${Domain}SaveReq req){
+        ${domain}Service.save(req);
         return new CommonResp<>();
     }
     @GetMapping("/query-list")
-    public CommonResp<PageResp<PassengerQueryResp>> query(@Valid PassengerQueryReq req){
+    public CommonResp<PageResp<${Domain}QueryResp>> query(@Valid ${Domain}QueryReq req){
         req.setMemberId(LoginMemberContext.getId());
-        return new CommonResp<>(passengerService.queryList(req));
+        return new CommonResp<>(${domain}Service.queryList(req));
     }
     @DeleteMapping("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable("id") Long id){
-        passengerService.delete(id);
+        ${domain}Service.delete(id);
         return new CommonResp<>();
     }
 }
--- generator/src/main/java/com/itxiaohao/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/itxiaohao/train/generator/gen/ServerGenerator.java
@@ -1,22 +1,24 @@
 package com.itxiaohao.train.generator.gen;
 
 import com.itxiaohao.train.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class ServerGenerator {
-    static String servicePath = "[module]/src/main/java/com/itxiaohao/train/[module]/service/";
+    static String serverPath = "[module]/src/main/java/com/itxiaohao/train/[module]/";
 
     static String pomPath = "generator/pom.xml";
     static {
-        new File(servicePath).mkdirs();
+        new File(serverPath).mkdirs();
     }
 
     /**
@@ -35,6 +37,11 @@ public class ServerGenerator {
         return node.getText();
     }
 
+    /**
+     * 获取参数
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         // 获取mybatis-generator
         String generatorPath = getGeneratorPath();
@@ -42,8 +49,8 @@ public class ServerGenerator {
         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
         System.out.println("module: " + module);
         // 替换module
-        servicePath = servicePath.replace("[module]", module);
-        System.out.println("servicePath:" + servicePath);
+        serverPath = serverPath.replace("[module]", module);
+        System.out.println("servicePath:" + serverPath);
         // 读取table节点（表名和对应的实体类名）
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
@@ -57,7 +64,7 @@ public class ServerGenerator {
         String Domain = domainObjectName.getText();
         // domain=member
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
-        // 表名
+        // url名（如果是两个单词以上，使用“-”连接）
         String do_main = tableName.getText().replaceAll("_", "-");
         // 组装参数
         Map<String, Object> param = new HashMap<>();
@@ -66,7 +73,25 @@ public class ServerGenerator {
         param.put("do_main", do_main);
         System.out.println("组装参数："+ param);
 
-        FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(servicePath + Domain + "Service.java", param);
+        gen(Domain, param, "service");
+        gen(Domain, param, "controller");
+    }
+
+    /**
+     * 通过参数和ftl模板，生成对应文件
+     * @param Domain
+     * @param param
+     * @param target
+     * @throws IOException
+     * @throws TemplateException
+     */
+    private static void gen(String Domain, Map<String, Object> param, String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String toPath = serverPath + target + "/";
+        new File(toPath).mkdirs();
+        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
+        String fileName = toPath + Domain + Target + ".java";
+        System.out.println("开始生成文件：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 }
