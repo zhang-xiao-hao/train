2070937fc06c72f9069f6e170dbee5435d43a8b5
zhang-xiao-hao <2314606193@qq.com>
2023-09-05 15:59:42 +0800

    代码生成器生成passenger.vue成功，支持只读控制（只读模式下，生成的vue没有新增、删除等修改数据库的功能）

--- generator/src/main/java/com/itxiaohao/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/itxiaohao/train/generator/gen/ServerGenerator.java
@@ -15,6 +15,8 @@ import java.util.*;
 
 
 public class ServerGenerator {
+    static boolean readOnly = false;
+    static String vuePath = "web/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/itxiaohao/train/[module]/";
 
     static String pomPath = "generator/pom.xml";
@@ -93,18 +95,26 @@ public class ServerGenerator {
         param.put("tableNameCn", tableNameCn);
         param.put("fieldList", fieldList);
         param.put("typeSet", typeSet);
+        param.put("readOnly", readOnly);
         System.out.println("组装参数："+ param);
         // 生成service
-        gen(Domain, param, "service", "service");
+//        gen(Domain, param, "service", "service");
         // 生成controller
-        gen(Domain, param, "controller", "controller");
+//        gen(Domain, param, "controller", "controller");
         // 生成req
-        gen(Domain, param, "req", "saveReq");
-        gen(Domain, param, "req", "queryReq");
+//        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "req", "queryReq");
         // 生成resp
-        gen(Domain, param, "resp", "queryResp");
+//        gen(Domain, param, "resp", "queryResp");
+        genVue(do_main, param);
     }
-
+    private static void genVue(String do_main, Map<String, Object> param) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig("vue.ftl");
+        new File(vuePath).mkdirs();
+        String fileName = vuePath + do_main + ".vue";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
+    }
     /**
      * 通过参数和ftl模板，生成对应文件
      * @param Domain
