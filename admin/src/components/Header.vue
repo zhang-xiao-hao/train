7b9b874137b0a4433a36ed65fb8920cc7cfab29d
zhang-xiao-hao <2314606193@qq.com>
2023-09-06 14:05:34 +0800

    创建admin后台管理模块（所有代码从web模块复制）

---
+++ admin/src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <div style="float: right; color: white;">
+      您好，{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white">
+        退出登录
+      </router-link>
+    </div>
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined/> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined/> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/ticket">
+        <router-link to="/ticket">
+          <user-outlined /> &nbsp; 余票查询
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import store from "@/store";
+import router from "@/router";
+export default defineComponent({
+  name: "the-header-view",
+  setup() {
+    let member = store.state.member;
+    const selectedKeys = ref([]);
+    // 监测路由变化
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true})
+    return {
+      selectedKeys,
+      member
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ admin/src/components/Sider.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-layout-sider width="200" style="background: #fff">
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        mode="inline"
+        :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/ticket">
+        <router-link to="/ticket">
+          <user-outlined /> &nbsp; 余票查询
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
+</template>
+<script>
+
+import {defineComponent, ref, watch} from "vue";
+import router from "@/router";
+export default defineComponent({
+  name: "the-sider-view",
+  setup() {
+    const selectedKeys = ref([]);
+    // 监测路由变化
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true})
+    return {
+      selectedKeys
+    };
+  },
+})
+</script>
+<style scoped>
+
+</style>
