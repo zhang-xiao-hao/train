3cfe2d96219225cba6559a6e627c8f0855948d8e
zhang-xiao-hao <2314606193@qq.com>
2023-09-06 20:32:00 +0800

    集成pinyin-pro，自动生成拼音和首字母

--- admin/src/views/main/station.vue
+++ admin/src/views/main/station.vue
@@ -31,19 +31,20 @@
         <a-input v-model:value="station.name" />
       </a-form-item>
       <a-form-item label="站名拼音">
-        <a-input v-model:value="station.namePinyin" />
+        <a-input v-model:value="station.namePinyin" disabled/>
       </a-form-item>
-      <a-form-item label="站名拼音首字母">
-        <a-input v-model:value="station.namePy" />
+      <a-form-item label="拼音首字母">
+        <a-input v-model:value="station.namePy" disabled/>
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
+import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import {pinyin} from "pinyin-pro";
 
 export default defineComponent({
   name: "station-view",
@@ -86,7 +87,12 @@ export default defineComponent({
       dataIndex: 'operation'
     }
     ];
-
+    watch(() => station.value.name, ()=>{
+      if (Tool.isNotEmpty(station.value.name)){
+        station.value.namePinyin = pinyin(station.value.name, {toneType: 'none'}).replace(" ", "")
+        station.value.namePy = pinyin(station.value.name, {pattern: 'first', toneType: 'none'}).replace(" ", "")
+      }
+    }, {immediate: true})
     const onAdd = () => {
       station.value = {};
       visible.value = true;
