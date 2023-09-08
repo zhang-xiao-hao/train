4e99efe878ec00a4e1b1799dbab5fb280686efab
zhang-xiao-hao <2314606193@qq.com>
2023-09-08 22:59:02 +0800

    自动计算停站时长

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -46,7 +46,7 @@
         <a-time-picker v-model:value="trainStation.outTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
       </a-form-item>
       <a-form-item label="停站时长">
-        <a-time-picker v-model:value="trainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
+        <a-time-picker v-model:value="trainStation.stopTime" valueFormat="HH:mm:ss" placeholder="请选择时间" disabled/>
       </a-form-item>
       <a-form-item label="里程（公里）">
         <a-input v-model:value="trainStation.km" />
@@ -62,6 +62,7 @@ import axios from "axios";
 import {pinyin} from "pinyin-pro";
 import StationSelectView from "@/components/station-select.vue";
 import TrainSelectView from "@/components/train-select.vue";
+import dayjs from 'dayjs';
 
 export default defineComponent({
   name: "train-station-view",
@@ -147,6 +148,18 @@ export default defineComponent({
       trainStation.value = {};
       visible.value = true;
     };
+
+    // 自动计算停车时长
+    watch(() => trainStation.value.inTime, ()=>{
+      let diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(dayjs(trainStation.value.inTime, 'HH:mm:ss'), 'seconds');
+      trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, {immediate: true});
+
+    // 自动计算停车时长
+    watch(() => trainStation.value.outTime, ()=>{
+      let diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(dayjs(trainStation.value.inTime, 'HH:mm:ss'), 'seconds');
+      trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
+    }, {immediate: true});
 
     const onEdit = (record) => {
       trainStation.value = window.Tool.copy(record);
