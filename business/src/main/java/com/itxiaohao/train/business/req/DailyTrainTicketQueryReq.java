3627ac1d63a36e451808ebe9d7b2dae673cf0652
zhang-xiao-hao <2314606193@qq.com>
2023-09-12 18:56:50 +0800

    增加车次余票管理条件查询

--- business/src/main/java/com/itxiaohao/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/itxiaohao/train/business/req/DailyTrainTicketQueryReq.java
@@ -3,9 +3,16 @@ package com.itxiaohao.train.business.req;
 import com.itxiaohao.train.common.req.PageReq;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @ToString
 public class DailyTrainTicketQueryReq extends PageReq {
-
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private String trainCode;
+    private String start;
+    private String end;
 }
